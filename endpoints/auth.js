74422c507bbcdab92004a7f0f43f03a8f61a147d
felipe45676-glitch <felipe45676@gmail.com>
2025-12-22 16:57:42 -0300

    fix hora chile

--- endpoints/auth.js
+++ endpoints/auth.js
@@ -6,7 +6,12 @@ const multer = require('multer');
 const { addNotification } = require("../utils/notificaciones.helper");
 const { sendEmail } = require("../utils/mail.helper");
 const useragent = require('useragent');
-const { createBlindIndex, verifyPassword, decrypt, encrypt, hashPassword } = require("../utils/seguridad.helper");
+const { createBlindIndex, verifyPassword, decrypt } = require("../utils/seguridad.helper");
+
+const getAhoraChile = () => {
+  const d = new Date();
+  return new Date(d.toLocaleString("en-US", {timeZone: "America/Santiago"}));
+};
 
 
 const TOKEN_EXPIRATION = 12 * 1000 * 60 * 60;
@@ -83,31 +88,6 @@ const generateAndSend2FACode = async (db, user, type) => {
     html: htmlContent
   });
 };
-
-router.get("/mantenimiento/migrar-pqc", async (req, res) => {
-  try {
-    const usuarios = await req.db.collection("usuarios").find().toArray();
-    let cont = 0;
-    for (let u of usuarios) {
-      const up = {};
-      if (u.pass && !u.pass.startsWith('$argon2')) up.pass = await hashPassword(u.pass);
-      if (u.nombre && !u.nombre.includes(':')) up.nombre = encrypt(u.nombre);
-      if (u.apellido && !u.apellido.includes(':')) up.apellido = encrypt(u.apellido);
-      if (u.mail && !u.mail.includes(':')) {
-        const cleanMail = u.mail.toLowerCase().trim();
-        up.mail = encrypt(cleanMail);
-        up.mail_index = createBlindIndex(cleanMail);
-      }
-      if (Object.keys(up).length > 0) {
-        await req.db.collection("usuarios").updateOne({ _id: u._id }, { $set: up });
-        cont++;
-      }
-    }
-    res.json({ success: true, message: `Migración finalizada. ${cont} registros procesados.` });
-    } catch (err) {
-  res.status(500).json({ error: err.message });
-}
-});
 
 router.get("/", async (req, res) => {
   try {
@@ -276,7 +256,8 @@ router.post("/login", async (req, res) => {
       });
     }
 
-    const now = new Date();
+    const now = getAhoraChile()
+
     let finalToken = null;
     let expiresAt = null;
 
@@ -446,7 +427,7 @@ router.post("/verify-login-2fa", async (req, res) => {
       }
 
       finalToken = crypto.randomBytes(32).toString("hex");
-      expiresAt = new Date(Date.now() + TOKEN_EXPIRATION);
+      expiresAt = new Date(now.getTime() + TOKEN_EXPIRATION);
 
       await req.db.collection("tokens").insertOne({
         token: finalToken,
